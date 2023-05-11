e43e40007d2770ef8ff73396a820f8b36b3a4f8b
Ikarosv <ikarovieiraa@gmail.com>
2023-05-11 18:58:45 -0300

    🤡 mock: criado mock da model getAll

---
+++ app/backend/src/tests/mocks/Team.mock.ts
@@ -0,0 +1,16 @@
+import Team from "../../database/models/Team";
+
+export const mockGetTeam = [
+  {
+    "id": 1,
+    "teamName": "Avaí/Kindermann"
+  },
+  {
+    "id": 2,
+    "teamName": "Bahia"
+  },
+  {
+    "id": 3,
+    "teamName": "Botafogo"
+  }
+]
