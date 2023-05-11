7bd65fff7d7f10285dfda02fc765b645b915b7fc
Ikarosv <ikarovieiraa@gmail.com>
2023-05-11 19:00:55 -0300

    ✨ feat: criado o model dos times

--- app/backend/src/database/models/Team.ts
+++ app/backend/src/database/models/Team.ts
@@ -1,16 +1,22 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from '.';
+import TeamInterface from '../../types/Teams';
 
-class Team extends Model {
+// export type TeamCreateAttr = Omit<TeamInterface, 'id'>;
+
+class Team extends Model<TeamInterface> implements TeamInterface {
   /**
    * Helper method for defining associations.
    * This method is not a part of Sequelize lifecycle.
    * The `models/index` file will call this method automatically.
    */
+  public id!: number;
+  public teamName!: string;
   // static associate(models) {
   //   // models.Team.hasMany(models.Match, { foreignKey: 'home_team_id', as: 'matches' });
   // }
 }
+
 Team.init({
   id: DataTypes.INTEGER,
   teamName: DataTypes.STRING,
