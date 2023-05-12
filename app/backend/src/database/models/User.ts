12b04417a0d69f607d94ac335c8f8c9f5a330da7
Ikarosv <ikarovieiraa@gmail.com>
2023-05-12 01:08:19 -0300

    ✨ feat: modelo de usuario criado

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,28 @@
+import { DataTypes, Model } from 'sequelize';
+import UserInterface from '../../types/Users';
+import sequelize from '.';
+
+class User extends Model<UserInterface> implements UserInterface {
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  underscored: true,
+  sequelize,
+  modelName: 'User',
+  tableName: 'users',
+  timestamps: false,
+});
